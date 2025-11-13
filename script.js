d38d3cdf25b7c4fb3e86fbe9813bde02874d97de
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-13 20:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let count=0;
+const para = document.querySelector("p");
+para.innerText = count;
+
+function incrementbtn() {
+	alert(count);
+	count++;
+	para.innerText = count;
+	
+}
